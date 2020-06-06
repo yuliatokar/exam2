4a3c1d24423749dad71bb268a699ca1805f93c55
yuliatokar <yulia.tokar.2000@gmail.com>
2020-06-06 16:58:58 +0300

    add

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+$(document).ready(function(){
+    $('.slider').slick({
+      slidesToShow: 5,
+      autoplay: true,
+      autoplaySpeed: 3002,
+      pauseOnHover: false,
+      dots: true
+    })
+  })
